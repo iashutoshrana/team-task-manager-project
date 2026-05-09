aff1bb8a259c2669d3435933ad65cd452842c99e
iashutoshrana <ashutoshrana1702@gmail.com>
2026-05-09 10:26:49 +0530

    fix cors policy

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,12 @@ require('./models/index')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'))
